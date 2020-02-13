2a1f1c6299873ab21bffea0e04eed7418e4a0108
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2020-02-13 16:16:58 +0800

    ceshi

---
+++ js/rem.js
@@ -0,0 +1,14 @@
+
+
+(function (doc, win, undefined) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in win? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (clientWidth === undefined) return;
+            docEl.style.fontSize = 100 * (clientWidth /375) + 'px';
+        };
+    if (doc.addEventListener === undefined) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
